7e8063d3674ea88609dca392061f439517b93143
Tautik Agrahari <tautikagrahari@gmail.com>
2023-04-28 22:33:53 +0530

    Added Routing on app.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,46 @@ import "./App.css";
 import Homepage from "./components/Homepage";
 import Navbar from "./components/Navbar";
 import background from "./assets/images/background.svg";
+import Error from "./components/Error";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Login } from "./components/Login";
 
-function App() {
+const App = () => {
   return (
     <>
-      <div className="relative ">
-        <img
-          className="absolute top-0 right-0  w-[75%] -z-10 mt-[-1.25rem] "
-          src={background}
-          alt=""
-        />
-        <div>
-          <Navbar />
-          <Homepage />
-        </div>
-      </div>
+      <RouterProvider router={routes} />
     </>
   );
-}
+};
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <div className="relative ">
+          <img
+            className="absolute top-0 right-0  w-[75%] -z-10 mt-[-1.25rem] "
+            src={background}
+            alt=""
+          />
+          <Navbar />
+          <Outlet />
+        </div>
+      </>
+    ),
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
 
 export default App;
