a1f02e46c5d427e664875d1d9e97f6cc69dade9e
Tautik Agrahari <tautikagrahari@gmail.com>
2023-04-29 15:01:15 +0530

    Added HowItWork page, routing left

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,5 +1,31 @@
+import AboutUsImg from "../assets/images/aboutUsImg.svg";
+import { blueButton, blackButton } from "./button";
 const AboutUs = () => {
-  return <div>AboutUs</div>;
+  return (
+    <div className="flex w-[80%] m-auto h-[100vh] ">
+      <div className="w-[60%] pl-10 -z-10 bg-amber-300 flex flex-col justify-center">
+        <h1 className="text-6xl	">
+          Build your <br /> audience and <br /> grow your brand
+        </h1>
+        <div className="left-0 py-4 ">
+          <p className="text-lg">
+            --no more, no less. Deploy from our single pane of
+            <br /> glass, manage them with ease and scale up as
+            <br /> fast as your workload grows.
+          </p>
+        </div>
+        <div className="flex">
+          {blueButton("Get Started")}
+          <button className="ml-4 text-secondary3 bg-secondary1 px-6 py-4 border rounded-full">
+            Watch Video
+          </button>
+        </div>
+      </div>
+      {/* <div className="w-[50%] bg-red-50 flex justify-center"> */}
+      <img className="relative z-10  w-[40rem]" src={AboutUsImg} alt="" />
+      {/* </div> */}
+    </div>
+  );
 };
 
 export default AboutUs;
--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,5 +1,67 @@
+import HowItWorkImg from "../assets/images/howItWorkImg.svg";
+import { blackButton } from "./button";
+const SectionWithButtons = () => {
+  return (
+    <div className="flex flex-row  mt-[-4rem] justify-between w-[80%] m-auto">
+      <button className="text-white bg-primary1 px-20 py-4 border rounded-xl">
+        01. BUILD
+      </button>
+      <button className="text-white bg-primary1 px-20 py-4 border rounded-xl">
+        02. STANDARDIZE
+      </button>
+      <button className="text-white bg-primary1 px-20 py-4 border rounded-xl">
+        03. INNOVATE
+      </button>
+    </div>
+  );
+};
+
+// Feature component
+const Feature = ({ title, description, buttonText }) => {
+  return (
+    <div className="flex  w-[70%] m-auto mt-10 ">
+      <div className=" flex-1 flex flex-col justify-evenly">
+        <h1 className="text-4xl">{title}</h1>
+        <p>{description}</p>
+        <div className="max-w-fit">{blackButton(buttonText)}</div>
+      </div>
+      <div className="flex-1">
+        <img src={HowItWorkImg} alt="" />
+      </div>
+    </div>
+  );
+};
+
+// Home page component
+const DyanmicContainer = () => {
+  return (
+    <div className="flex flex-wrap">
+      <SectionWithButtons />
+      <Feature
+        title="Build Infrastructure Automation"
+        description="Infrastructure automation process is used by IT operations and development teams to compose, collaborate, reuse, and provision infrastructure as code."
+        buttonText="Get Started"
+      />
+    </div>
+  );
+};
 const HowItWorks = () => {
-  return <div>HowItWorks</div>;
+  return (
+    <div className="min-h-[100vh]  bg-gray-100 flex flex-col justify-evenly items-center">
+      <div>
+        <button className="text-white bg-primary1 px-10 py-4 border rounded-full ml-4">
+          Features
+        </button>
+      </div>
+      <h1 className="text-center mt-[-4rem] text-4xl">
+        Work smarter <br />
+        with easy deploys, manages & scales
+      </h1>
+      <div className=" flex flex-wrap   ">
+        <DyanmicContainer />
+      </div>
+    </div>
+  );
 };
 
 export default HowItWorks;
