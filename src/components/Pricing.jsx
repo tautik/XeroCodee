52f4dbb1e408c2f43cc3875af7de9d248548753d
Tautik Agrahari <tautikagrahari@gmail.com>
2023-04-29 17:33:22 +0530

    Added basic Testimonal and and Pricing section

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,5 +1,73 @@
+import { blackButton, blueButton } from "./button";
+import UpiCard from "../assets/images/upiCard.svg";
+const PricingContent = () => {
+  return (
+    <div className="w-[20rem] h-[30rem] flex flex-col justify-evenly pr-8">
+      <div className="w-32">{blueButton("pricing")}</div>
+      <h1 className="text-4xl">
+        Simple and <br /> flexible pricing
+      </h1>
+      <p>
+        It is a long established fact that a reader the will be distracted by
+        the readable content of a page from when looking at it layout.{" "}
+      </p>
+      <h2 className="text-xl">Accepted Payment Methods</h2>
+      <img src={UpiCard} alt="" />
+    </div>
+  );
+};
+
+const BusinessCard = () => {
+  return (
+    <div className="bg-secondary1 border border-black w-[20rem] h-[30rem] flex flex-col justify-evenly divide-y  divide-black mr-4">
+      <div className="flex pl-5 items-center  justify-around ">
+        <h1 className="text-4xl">Buisness</h1>
+        <h3>Save 30%</h3>
+      </div>
+      <div className="text-center pt-4">
+        <h1 className="font-semibold">
+          <span className=" text-3xl">$49.99</span> / month
+        </h1>
+        <div>
+          <h3 className="rounded-full mt-2 bg-[#FFEECC] text-[#C68A15] w-60 m-auto">
+            Billed as $549.99 per year
+          </h3>
+        </div>
+      </div>
+      <div className=" flex flex-col items-center justify-evenly h-80">
+        <h1>
+          <span className="font-bold">5+ Kubernetes</span> Cluster
+        </h1>
+        <h1>
+          <span className="font-bold">API</span> Gateway
+        </h1>
+        <h1>Analytics & Security</h1>
+        <h1>
+          <span className="font-bold">SSH</span> key management
+        </h1>
+        <h1>VPC Peering</h1>
+        <div className="w-44 text-center mt-4 ">
+          {blackButton("Get Started")}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const Pricing = () => {
-  return <div>Pricing</div>;
+  return (
+    <div className="min-h-screen flex flex-row justify-center items-center">
+      <div>
+        <PricingContent />
+      </div>
+      <div>
+        <BusinessCard />
+      </div>
+      <div>
+        <BusinessCard />
+      </div>
+    </div>
+  );
 };
 
 export default Pricing;
--- src/components/Testimonal.jsx
+++ src/components/Testimonal.jsx
@@ -1,5 +1,61 @@
+import { useState } from "react";
+import testimonalImg from "../assets/images/testimonalImg.svg";
+import Star from "../assets/images/Star.svg";
+
+const UserInfo = ({ designation, name }) => {
+  return (
+    <div className="flex flex-col justify-between items-center mt-4">
+      <img className="rounded-full" src={testimonalImg} alt="" />
+      <h1 className="text-2xl bold">{name}</h1>
+      <img src={Star} alt="" />
+    </div>
+  );
+};
+
+// Feature component
+const Review = ({ text }) => {
+  return (
+    <div className="flex flex-col justify-center  items-center w-[70%] m-auto mt-10">
+      <p className="text-center">{text}</p>
+      <UserInfo name="James Toriff" designation="Developer" />
+    </div>
+  );
+};
+
+// Home page component
+const DyanmicContainer = () => {
+  return (
+    <div className="flex flex-wrap">
+      <Review
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit facere
+        quae perspiciatis doloribus sit fugit blanditiis debitis quo, ab nemo
+        quod officia? Praesentium corrupti molestiae amet consectetur obcaecati
+        eos temporibus!"
+      />
+    </div>
+  );
+};
 const Testimonal = () => {
-  return <div>Testimonal</div>;
+  return (
+    <div className="min-h-[100vh] text-white bg-primary1 flex flex-col justify-evenly items-center">
+      <div>
+        <button className=" bg-primary1 px-10 py-4 border rounded-full ml-4">
+          Testimonial
+        </button>
+      </div>
+      <h1 className="text-center mt-[-4rem] text-4xl">
+        Trusted by millions of creators.
+      </h1>
+      <div className=" flex flex-col flex-wrap items-center justify-center w-[80%] mx-auto  ">
+        <h1 className="text-3xl font-extrabold">"</h1>
+        <div className="flex ">
+          <button>left</button>
+          <DyanmicContainer />
+          <button>right</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonal;
