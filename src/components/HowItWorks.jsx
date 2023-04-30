193cf3ec743c0919fbce6c0fd46e983e70d5c7e7
Tautik Agrahari <tautikagrahari@gmail.com>
2023-04-30 16:04:50 +0530

    fixed How it works section

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -17,7 +17,7 @@ const SectionWithButtons = () => {
     setButtonClicked(value);
   };
   return (
-    <div className="flex flex-row flex-wrap mt-[-4rem] justify-between w-[60%] m-auto font-bold text-sm">
+    <div className="flex text-lg font-bold flex-row flex-wrap mt-[-4rem] justify-between w-[75%] m-auto ">
       <button
         className={` ${
           buttonClicked === "BUILD" ? styleClicked : notClick
@@ -50,13 +50,13 @@ const SectionWithButtons = () => {
 const Working = ({ title, description, buttonText }) => {
   return (
     <div className="flex  w-[70%] m-auto mt-10 ">
-      <div className=" flex-1 flex flex-col justify-evenly">
+      <div className=" flex-1 flex flex-col justify-evenly mr-10">
         <div className="flex flex-row">
           <img className="w-[5rem] mr-5" src={howItWorkProfile} alt="" />
-          <h1 className="text-4xl">{title}</h1>
+          <h1 className="text-3xl font-bold">{title}</h1>
         </div>
 
-        <p>{description}</p>
+        <p className="text-secondary4 text-lg">{description}</p>
         <div className="max-w-fit">{blackButton(buttonText)}</div>
       </div>
       <div className="flex-1">
@@ -108,12 +108,12 @@ const HowItWorks = () => {
       <div>
         <button
           disabled
-          className=" text-white font-bold bg-primary1 px-10 py-4 border rounded-full ml-4"
+          className=" text-white  font-bold bg-primary1 px-10 py-4 border rounded-full ml-4"
         >
           How It Works
         </button>
       </div>
-      <h1 className="text-center font-extrabold mt-[-4rem] text-4xl">
+      <h1 className="text-center text-5xl font-extrabold mt-[-4rem] ">
         Work smarter <br />
         with easy deploys, manages & scales
       </h1>
